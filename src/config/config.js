3578e397a55b357032044eec263699cb1fa0d59f
TejasPatel007 <pateltejas9396@yahoo.com>
2024-12-27 22:27:36 -0500

    [TP} : changed backend server URL

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ const config = {
     process.env.NEXT_PUBLIC_BACKEND_API_URL && process.env.NEXT_PUBLIC_BACKEND_API_URL.trim() !== ""
       ? process.env.NEXT_PUBLIC_BACKEND_API_URL+`:8080` // Use the custom API URL from environment variable if not empty
       : isBrowser
-      ? `http://3.95.193.2`+`:8080` // Replace port 3000 with 8080
+      ? `http://107.22.68.106`+`:8080` // Replace port 3000 with 8080
       : "http://localhost"+`:8080`, // Default fallback for server-side
 };
 
