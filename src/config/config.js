2a1bfc85e0b8eb53227281a9fb409765fb27438b
lenin12345-dev <leninmohapatra@gmail.com>
2025-01-04 17:07:49 -0500

    link add

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,7 @@
 const isBrowser = typeof window !== "undefined"; // Check if running in the browser
 console.log("process.env.NEXT_PUBLIC_BACKEND_API_URL:::::",process.env.NEXT_PUBLIC_BACKEND_API_URL)
 const config = {
-  API_URL:
-    process.env.NEXT_PUBLIC_BACKEND_API_URL && process.env.NEXT_PUBLIC_BACKEND_API_URL.trim() !== ""
-      ? process.env.NEXT_PUBLIC_BACKEND_API_URL+`:8080` // Use the custom API URL from environment variable if not empty
-      : isBrowser
-      ? `http://3.95.193.2`+`:8080` // Replace port 3000 with 8080
-      : "http://localhost"+`:8080`, // Default fallback for server-side
+  API_URL:"https://c229-54-84-209-58.ngrok-free.app"
 };
 
 export default config;
